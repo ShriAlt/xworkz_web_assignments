584e044b25b26309a9f7268e68a4e09fbc7c0d65
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-16 22:33:38 +0530

    updating

---
+++ friday/new.js
@@ -0,0 +1,20 @@
+var age=455454
+var name="sdfsd"
+let age1=455454
+document.write("hello world")
+
+document.write(age)
+const age=455454
+
+let emp1={id:17,employeeName:'aa',salary:45555}
+let emp2 =new Object();
+emp2.id=23;
+emp2.name="harsga";
+emp2.salary=48528451;
+
+function emp3(id,namme,salry){
+    this.id=id;
+    this.name=namme;
+    this.salary=salry;
+
+}
