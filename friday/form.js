83bec39f798386e12ea4baabfdf7e92e004b9afc
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-24 21:38:46 +0530

    updating

---
+++ friday/form.js
@@ -0,0 +1,11 @@
+// function onClick(a,b){
+//  alert("hi ani")
+// }
+function submitForm(){
+    let value =true;
+    let nameInput=document.formName.namename.value;
+    // alert(nameInput)
+    let numberInput=document.formName.numbernumber .value;
+    // alert(numberInput)
+    return value;
+}
