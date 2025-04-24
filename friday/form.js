345b1fa51058245cf4d807f689ab81aa97b553b9
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-25 00:12:53 +0530

    updating web assignment

--- friday/form.js
+++ friday/form.js
@@ -7,5 +7,14 @@ function submitForm(){
     // alert(nameInput)
     let numberInput=document.formName.numbernumber .value;
     // alert(numberInput)
+    let emailInout=document.formName.emailEmail .value;
+    // alert(emailInout)
+    let passwordInput=document.formName.passwordPassword .value;
+    // alert(passwordInput)
+    let cpasswordInput=document.formName.cpasswordCinput .value;
+    // alert(cpasswordInput)
+    let genderInput=document.formName.gender .value;
+    // alert(genderInput)
     return value;
+
 }
