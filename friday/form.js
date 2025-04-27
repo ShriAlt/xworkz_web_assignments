1be8963b76ad88a27240cbbd440dbd9fb70a6ba1
Shriharsha K M <shriharshakm10@gmail.com>
2025-04-27 23:44:38 +0530

    updating

--- friday/form.js
+++ friday/form.js
@@ -1,22 +1,21 @@
-// function onClick(a,b){
-//  alert("hi ani")
-// }
+function onClick(a,b){
+}
 function submitForm() {
     let val = true;
     let nameInput = document.formName.namename.value;
     let numberInput = document.formName.numbernumber.value;
     let ageInput = document.formName.ageage.value;
-    // if (nameInput.length < 3 || nameInput.length > 20) {
-    //     val = false;
+    if (nameInput.length < 3 || nameInput.length > 20) {
+        val = false;
 
-    // }
+    }
 
-    // if (nameInput.length>3 || nameInput.length <20) {
-    //     val=true
-    // } else {
-    //     val=false
+    if (nameInput.length>3 || nameInput.length <20) {
+        val=true
+    } else {
+        val=false
 
-    // }
+    }
 
     if (3<=nameInput.length<=20 && ageInput>=18 && numberInput.length == 10 ) {
     }
